38f21efdb075613d58417583df2c5d1520b7cba4
jckhe <iamjckhe@gmail.com>
2022-12-03 13:06:51 -0800

    deploying

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, '../build')));
 
