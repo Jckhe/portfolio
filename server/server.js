90fcea4def2a3711fb4fa0ea1d04ca20323d0f0e
jckhe <iamjckhe@gmail.com>
2022-12-05 17:02:00 -0800

    SSL Verification

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.use('/', express.static(path.join(__dirname, '../build')));
 
 
-app.use('/.well-known/pki-validation/7318326D7CF54A243A669468BDE3E2BC.txt', (req, res) => {
-  res.sendFile(path.join(__dirname, '../7318326D7CF54A243A669468BDE3E2BC.txt'));
-})
+// app.use('/.well-known/pki-validation/7318326D7CF54A243A669468BDE3E2BC.txt', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../7318326D7CF54A243A669468BDE3E2BC.txt'));
+// })
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
